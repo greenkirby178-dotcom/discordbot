43d2dff7e9047444af94a347d76f814af79ec042
greenkirby178-dotcom <greenkirby178@gmail.com>
2026-03-05 15:45:32 +0900

    Add files via upload

--- bot.py
+++ bot.py
@@ -20,6 +20,14 @@ async def totime(interaction: discord.Interaction):
     result = random.choice([str(n) + "秒","追放",str(n) + "秒",str(n) + "秒",str(n) + "秒",])
     await interaction.response.send_message(result)
 
+@tree.command(name="cc",description="既存48コースの中から一つコースを選びます")
+async def cc(interaction: discord.Interaction):
+    course_list = ["mks","wp","ssc","tr","mc","th","tm","sgf","sa","ds","ed","mw","cc","bdd","bc","rr","yc","ea","dd","dmc",
+    "bp","cl","ww","ac","rmmm","rmc","rccb","rtt","rddd","rdp3","rRRy","rdkj","rwc","rsl","rmp","ryv","rttc","rpps","rgv",
+    "rrrd","dwgm","drr","diio","dhc","dnbc","drir","dsbs","dbb"]
+    result = random.choice(course_list)
+    await interaction.response.send_message(result)
+
 @client.event
 async def on_ready():
     await tree.sync()
