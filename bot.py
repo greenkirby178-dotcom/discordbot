6b9e6cccb7c655c4c585a618b6bb510d3bed3cd3
greenkirby178-dotcom <greenkirby178@gmail.com>
2026-03-05 13:31:09 +0900

    Add files via upload

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ tree = app_commands.CommandTree(client)
 
 @tree.command(name="to", description="toするかしないかを決めます")
 async def to(interaction: discord.Interaction):
-    result = random.choice(["toする", "toしない"])
+    result = random.choice(["toする", "toしない","toする", "toする", "toする", "toする", "toする", "toする", "toする", "toする", ])
     await interaction.response.send_message(result)
 
 @tree.command(name="totime", description="時間をランダムで決めます")
