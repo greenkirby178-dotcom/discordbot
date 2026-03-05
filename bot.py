fdb3faff8ecb8ae6b23deeba175e808b9f203df3
greenkirby178-dotcom <greenkirby178@gmail.com>
2026-03-05 22:08:24 +0900

    Add files via upload

--- bot.py
+++ bot.py
@@ -28,6 +28,25 @@ async def cc(interaction: discord.Interaction):
     result = random.choice(course_list)
     await interaction.response.send_message(result)
 
+tree.command(name="dlcc",description="dlc48コースの中から一つコースを選びます")
+async def dlcc(interaction: discord.Interaction):
+    course_list = ["bpp","btc","bcmo","bcma","btb","bsr","bsg","bnh","bnym","bmc3","bkd","bwp","bsst","bsl","bmg","bshs","bll","bbl","brrm","bmt",
+    "bbb","bpg","bmm","brr7","bad","brp","dks","yi","bbr","bmc","bws","bssy","batd","bdcr","bmh","bscs","blal","bsw","bkc",
+    "vv","bra","dkm","ddtc","dppc","bmd","briw","bbc3","brrw"]
+    result = random.choice(course_list)
+    await interaction.response.send_message(result)
+
+tree.command(name="allcc",description="96コースの中から一つコースを選びます")
+async def allcc(interaction: discord.Interaction):
+    course_list = ["mks","wp","ssc","tr","mc","th","tm","sgf","sa","ds","ed","mw","cc","bdd","bc","rr","yc","ea","dd","dmc",
+    "bp","cl","ww","ac","rmmm","rmc","rccb","rtt","rddd","rdp3","rRRy","rdkj","rwc","rsl","rmp","ryv","rttc","rpps","rgv",
+    "rrrd","dwgm","drr","diio","dhc","dnbc","drir","dsbs","dbb","bpp","btc","bcmo","bcma","btb","bsr","bsg","bnh","bnym","bmc3","bkd","bwp","bsst","bsl","bmg","bshs","bll","bbl","brrm","bmt",
+    "bbb","bpg","bmm","brr7","bad","brp","dks","yi","bbr","bmc","bws","bssy","batd","bdcr","bmh","bscs","blal","bsw","bkc",
+    "vv","bra","dkm","ddtc","dppc","bmd","briw","bbc3","brrw"]
+    result = random.choice(course_list)
+    await interaction.response.send_message(result)
+
+
 @client.event
 async def on_ready():
     await tree.sync()
